6bc6e3c23a7d3e66bddb2cfd3638caf4178ae180
Alex Darvin <amdarvin14@gmail.com>
2020-09-25 19:54:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   def execute_transaction
     #sender withdraws money and sends it to receiver; receiver adds money to their balance
     #must be valid(refer to bank_account valid? method)
-    if @sender.balance > 0 && @sender
+    if @sender.balance > @amount && @status == "pending"
       receiver.balance += sender.amount
     end
   end
